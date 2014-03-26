0fc87925fb0d075ac94b579a1214892ab6607162
Dan Keiger <dankeiger@gmail.com>
2014-03-25 20:02:05 -0400

    basic message tab generation

--- src/com/example/howler/MessagesList.java
+++ src/com/example/howler/MessagesList.java
@@ -1,16 +1,29 @@
 package com.example.howler;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MessagesList extends Activity {
-
+	
+	private List<String> messageList;
+	private LinearLayout main;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,7 +31,8 @@ public class MessagesList extends Activity {
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);		
 		setContentView(R.layout.activity_messages_list);
-		
+		this.PopulateMessageList();
+		this.DisplayMessageList();
 		//get the message button and make it not pressable
 		Button messageButton = (Button) findViewById(R.id.message_list_button);
 		messageButton.setEnabled(false);	
@@ -33,7 +47,35 @@ public class MessagesList extends Activity {
 			}
 		});
 	}
-
+	public void PopulateMessageList(){
+		File audioPath = new File(Environment.getDataDirectory().getAbsolutePath() + "/data/com.example.howler/");
+		messageList = Arrays.asList(audioPath.list(
+				new FilenameFilter(){
+					public boolean accept(File audioPath, String audioName){
+						return audioName.endsWith(".3gp");
+					}
+				}
+			));
+		Toast.makeText(MessagesList.this, "Messages that exist:  " + messageList, Toast.LENGTH_LONG).show();
+	}
+	public void DisplayMessageList(){
+		main =(LinearLayout) findViewById(R.id.messages);
+		for(final String message : messageList){
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+			LinearLayout layout = new LinearLayout(getApplicationContext());
+			LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+			
+			layout.setOrientation(LinearLayout.VERTICAL);
+			layout.setLayoutParams(params);
+			Button btnMessage = new Button(getApplicationContext());
+			btnMessage.setLayoutParams(buttonParams);
+			btnMessage.setText(message);
+			
+			layout.addView(btnMessage);
+			main.addView(layout);
+			
+		}	
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
